cabadcd2ef50749f39116bd8d93ffb8ca326628b
nbingo <5738735+nbingo@users.noreply.github.com>
2022-02-26 16:55:42 -0800

    Fixed params to harness

--- src/main.py
+++ src/main.py
@@ -41,5 +41,5 @@ if __name__ == "__main__":
         num_machines=args.num_machines,
         machine_rank=args.machine_rank,
         dist_url=args.dist_url,
-        args=(args,),
+        args=(),
     )
