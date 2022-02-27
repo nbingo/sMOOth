294043d6574787ff9cdd50bc3fbc88e841be6965
nbingo <5738735+nbingo@users.noreply.github.com>
2022-02-26 17:35:55 -0800

    Fixed config import

--- src/configs/adult/adult_mlp_single_mp.py
+++ src/configs/adult/adult_mlp_single_mp.py
@@ -1,9 +1,8 @@
 # won't be editing all of these, but we need them to be imported so that they can be loaded in the config
-from adult_mlp_standard import (
-dataloader,
-train,
-model,
-optimizer,
-lr_multiplier
+from .adult_mlp_standard import (
+    dataloader,
+    train,
+    model,
+    optimizer,
+    lr_multiplier
 )
-
