976c7eaeea813385df0a72424ef13a7733ae03bb
nbingo <5738735+nbingo@users.noreply.github.com>
2022-03-01 09:11:10 -0800

    fixed grouper

--- src/loaders/wilds_fmow_loader.py
+++ src/loaders/wilds_fmow_loader.py
@@ -6,14 +6,14 @@ from torch.utils.data import Dataset
 
 class WildsFMoWDataset(Dataset):
     def __init__(self, subset: str):
-        self.dataset = get_dataset(dataset="fmow", download=True)
-        # Need to normalize lilke this for pre-trained efficient net from pytorch
+        self.full_dataset = get_dataset(dataset="fmow", download=True)
+        # Need to normalize like this for pre-trained efficient net from pytorch
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        self.dataset = self.dataset.get_subset(
+        self.dataset = self.full_dataset.get_subset(
             subset,
             transform=transforms.Compose([transforms.Resize((448, 448)), self.normalize, transforms.ToTensor()]),
         )
-        self.grouper = CombinatorialGrouper(self.dataset, ['region'])
+        self.grouper = CombinatorialGrouper(self.full_dataset, ['region'])
 
     def __len__(self):
         return len(self.dataset)
